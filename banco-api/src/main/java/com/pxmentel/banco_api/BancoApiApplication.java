f0e861d5d696a97fc11f793c17ad4576a0e26dee
Vinicius Pimentel <viniciuspimentel0@hotmail.com>
2026-02-17 19:16:15 -0300

    ajustes

--- banco-api/src/main/java/com/pxmentel/banco_api/BancoApiApplication.java
+++ banco-api/src/main/java/com/pxmentel/banco_api/BancoApiApplication.java
@@ -9,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class git aBancoApiApplication {
+public class BancoApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BancoApiApplication.class, args);
